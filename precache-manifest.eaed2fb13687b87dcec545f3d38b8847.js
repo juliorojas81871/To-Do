cd40e0e026c1cab7994a14dc3735c36f2504eb36
Julio Rojas <juliorojas81871@gmail.com>
2022-08-06 20:43:31 -0400

    Updates

---
+++ precache-manifest.eaed2fb13687b87dcec545f3d38b8847.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b75c2ebfec903a9d954d4c96bca9c48",
+    "url": "/To-Do/index.html"
+  },
+  {
+    "revision": "eef810e984a3eb8b89a9",
+    "url": "/To-Do/static/js/2.08ea41aa.chunk.js"
+  },
+  {
+    "revision": "6e043b2ea41e41d4fc43",
+    "url": "/To-Do/static/js/main.90bf282d.chunk.js"
+  },
+  {
+    "revision": "6f626715d01e04ad40e0",
+    "url": "/To-Do/static/js/runtime-main.f1da648c.js"
+  }
+]);
